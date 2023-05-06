fee8374e08227d971c1093f197b07ced76b6208b
Sean Wang <seanwaterloo2997@gmail.com>
2023-05-06 09:09:30 -0400

    Update App.js

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, TouchableWithoutFeedback, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View, Image, Button, TouchableWithoutFeedback, Alert, SafeAreaView } from 'react-native';
 
 export default function App() {
   console.log("Hello world")
 
   return (
     <SafeAreaView style={styles.container}>
-      
+      <Button title="Hit on me" onPress={() => { Alert.alert("Alert!", "If I'm left single again, will (can) you go to prom with me this june?", [
+        { text: "Yes"},
+        { text: "No"}
+      ]) }} />
     </SafeAreaView>
   );
 }
